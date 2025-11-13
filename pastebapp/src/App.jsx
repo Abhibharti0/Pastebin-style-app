7b4f11c6b192597604ae18839a18aaa8ab568c04
Abhishek Bharti <abhibharti457@gmail.com>
2025-11-13 15:33:08 +0530

    route

--- pastebapp/src/App.jsx
+++ pastebapp/src/App.jsx
@@ -1,11 +1,50 @@
 import { useState } from 'react'
+import "./App.css"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Navbar from './assets/components/Navbar'
+import Home from './assets/components/Home'
+import ViewPaste from './assets/components/ViewPaste'
+import Pastes from './assets/components/Pastes'
+
+
+const router =createBrowserRouter(
+[
+  {
+    path:"/",
+    element:
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  },
+  {
+    path:"/",
+    element:
+    <div>
+       <Navbar />
+      <Pastes />
+    </div>
+  },
+  {
+    path:"/",
+    element:
+    <div>
+       <Navbar />
+      <ViewPaste />
+    </div>
+  }
+]
+)
 
 
 function App() {
   
 
   return (
-    <div>h</div>
+    <div>
+      <RouterProvider router={router} />
+
+    </div>
   )
 }
 
